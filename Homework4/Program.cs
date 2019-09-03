7fbcc8c6e7a5027ce55f7268c33c5a9a19f2ecd7
Cosmin Balosache <cbalosache@spartaglobal.com>
2019-09-03 08:58:19 +0100

    Prototype

---
+++ Homework4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = "Bob";
+            string nameTwo = "Carly";
+            Console.WriteLine(Comparison.CompareByName(nameOne, nameTwo));
+            Console.WriteLine(Comparison.CompareByLength(nameOne,nameTwo));
+            nameOne = "Carly";
+            nameTwo = "Carly";
+            Console.WriteLine(Comparison.CompareByName(nameOne, nameTwo));
+            Console.WriteLine(Comparison.CompareByLength(nameOne, nameTwo));
+            nameOne = "Edward";
+            nameTwo = "Carly";
+            Console.WriteLine(Comparison.CompareByName(nameOne, nameTwo));
+            Console.WriteLine(Comparison.CompareByLength(nameOne, nameTwo));
+            Console.Read();
+        }
+    }
+    class Comparison
+    {
+        public string Name { get; set; }
+
+        interface ICompareByName
+        {
+	        int CompareByName();
+        }
+        interface ICompareByLength
+        {
+	        int CompareByLength();
+        }
+        public static int CompareByName(string name1, string name2)
+        {
+            return string.Compare(name1, name2);
+        }
+        public static int CompareByLength(string name1, string name2)
+        {
+            if(name1.Length == name2.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                if(name1.Length > name2.Length)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
